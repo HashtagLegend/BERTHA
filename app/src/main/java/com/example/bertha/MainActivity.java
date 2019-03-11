00e473e8861033bd5d81f4983ac7f8bd34ee042b
Patrick Thomas Ørum <Patr421b@edu.easj.dk>
2019-03-11 20:29:06 +0100

    REST API implemented - not working!

--- app/src/main/java/com/example/bertha/MainActivity.java
+++ app/src/main/java/com/example/bertha/MainActivity.java
@@ -1,22 +1,119 @@
 package com.example.bertha;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.bertha.Model.Data;
+import com.example.bertha.REST.ReadHttpTask;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String MINE = "MINE";
+    public static final String urlWristbandData = "https://berthawristbandrestprovider.azurewebsites.net/api/wristbanddata/get.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void goToLogin(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
 
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    public void getData(View view) {
+        AsyncTask task = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                Log.d(MINE, "doInBackground: clicked");
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(urlWristbandData)
+                        .build();
+                Response response = null;
+
+                try{
+                    response = client.newCall(request).execute();
+                    return response.body().string();
+                }
+                catch(IOException e){
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                Log.d(MINE, "onPostExecute: " + o);
+            }
+        }.execute();
+
+    }
+
+    /*
+    private class ReadFromWristbandTask extends ReadHttpTask {
+
+
+
+        @Override
+        protected void onPostExecute(CharSequence jsonString) {
+            Log.d(MINE, "onPostExecute: called");
+            Gson gson = new GsonBuilder().create();
+            final Data data = gson.fromJson(jsonString.toString(), Data.class);
+
+
+            ListView list = findViewById(R.id.mainDataLV);
+            ArrayAdapter adapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_list_item_1, (List<Data>) data);
+
+
+            list.setAdapter(adapter);
+
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    //TODO: Create ArrayAdapter and create onItemClick method
+
+                Intent intent = new Intent(getBaseContext(), BookActivity.class);
+                Book book = (Book) parent.getItemAtPosition(position);
+                intent.putExtra(BookActivity.BOOK, book);
+                startActivity(intent);
+
+                }
+
+            });
+
+
+            Log.d(MINE, "onPostExecute: " + data.toString());
+
+        }
+
+    }
+    */
 }
---
+++ app/src/main/java/com/example/bertha/Model/Data.java
@@ -0,0 +1,100 @@
+package com.example.bertha.Model;
+
+public class Data {
+
+    private String deviceId;
+    private int pm25, pm10, co2, o3, pressure, temperature, humidity;
+
+    public Data(String deviceId, int pm25, int pm10, int co2, int o3, int pressure, int temperature, int humidity) {
+        this.deviceId = deviceId;
+        this.pm25 = pm25;
+        this.pm10 = pm10;
+        this.co2 = co2;
+        this.o3 = o3;
+        this.pressure = pressure;
+        this.temperature = temperature;
+        this.humidity = humidity;
+    }
+
+    public Data(){
+
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public int getPm25() {
+        return pm25;
+    }
+
+    public void setPm25(int pm25) {
+        this.pm25 = pm25;
+    }
+
+    public int getPm10() {
+        return pm10;
+    }
+
+    public void setPm10(int pm10) {
+        this.pm10 = pm10;
+    }
+
+    public int getCo2() {
+        return co2;
+    }
+
+    public void setCo2(int co2) {
+        this.co2 = co2;
+    }
+
+    public int getO3() {
+        return o3;
+    }
+
+    public void setO3(int o3) {
+        this.o3 = o3;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "deviceId='" + deviceId + '\'' +
+                ", pm25=" + pm25 +
+                ", pm10=" + pm10 +
+                ", co2=" + co2 +
+                ", o3=" + o3 +
+                ", pressure=" + pressure +
+                ", temperature=" + temperature +
+                ", humidity=" + humidity +
+                '}';
+    }
+}
